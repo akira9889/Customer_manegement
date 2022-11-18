cea3318a5ba9635ff4b172ef6b3e674a4e92a186
akira9889 <aki_badmin89@icloud.com>
2022-11-18 14:50:35 +0900

    PDO関連のクラスの追加

---
+++ src/php/lib/Connection.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+require('/var/www/html/vendor/autoload.php');
+
+
+Dotenv\Dotenv::createImmutable('/var/app/customer_manegement')->load();
+
+class Connection
+{
+
+    protected readonly PDO $pdo;
+
+    private readonly string $dsn;
+
+    private readonly string $db_user;
+
+    private readonly string $db_pass;
+
+    private readonly array $options;
+
+    public function __construct()
+    {
+
+        $this->dsn = 'mysql:dbname=' . $_ENV['DB_NAME'] . ';host=' . $_ENV['DB_HOST'] . ';charset=utf8';
+        $this->db_user = $_ENV['DB_USER'];
+        $this->db_pass = $_ENV['DB_PASS'];
+        $this->options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ];
+
+        try {
+            $this->pdo = new PDO($this->dsn, $this->db_user, $this->db_pass, $this->options);
+        } catch (PDOException $e) {
+            echo "接続失敗: " . $e->getMessage() . "\n";
+            exit();
+        }
+    }
+}
---
+++ src/php/lib/ExecuteMySql.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+require_once(__DIR__ . '/./Connection.php');
+
+
+final class ExecuteMySql extends Connection {
+    
+    private readonly array $result;
+    
+    final public function __construct(private string $query, private array|null $options = null)
+    {
+        parent::__construct();
+
+        if ($this->options) $this->prepareExecute();
+
+        if (!$this->options) $this->queryExecute();
+    }
+
+    final public function execute() {
+        return $this->result;
+    }
+
+    // private methods
+
+    private function queryExecute() {
+        $stmt = $this->pdo->query($this->query);
+
+        $this->result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    private function prepareExecute() {
+        $stmt = $this->pdo->prepare($this->query);
+
+        foreach($this->options as $option_key => &$option_value) {
+            $stmt->bindParam(":$option_key", $option_value, $this->chackType($option_value));
+        }
+        
+        $stmt->execute();
+
+        $this->result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    private function chackType($option_value) {
+        if (gettype($option_value) === "integer") return PDO::PARAM_INT;
+        if (gettype($option_value) === "string")  return PDO::PARAM_STR;
+        if (gettype($option_value) === "boolean") return PDO::PARAM_BOOL;
+    }
+}
