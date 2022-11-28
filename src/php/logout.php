ae2708e86b2978d0d52ac98aac4d88da27e78927
akira9889 <aki_badmin89@icloud.com>
2022-11-28 15:09:40 +0900

    ログアウト機能の実装

--- src/php/logout.php
+++ src/php/logout.php
@@ -3,8 +3,14 @@ require_once('functions.php');
 
 session_start();
 
-$_SESSION = array();
-
-session_destroy();
-
-redirect('/login.php');
+if (isset($_SESSION['USER']['admin'])) {
+    $redirect_url = '/login.php?company_id=' . $_SESSION['USER']['id'];
+    sessionDestroy();
+    redirect($redirect_url);
+} elseif(isset($_SESSION['USER'])) {
+    $redirect_url = '/shop_login.php?shop_id=' . $_SESSION['USER']['shop_id'];
+    sessionDestroy();
+    redirect($redirect_url);
+} else {
+    throw new Exception('未ログイン時のログアウト', '500');
+}
